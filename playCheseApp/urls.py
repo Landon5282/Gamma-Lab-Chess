e38d0082c22a18e623baa38cdeadd47946d18244
Landon5282 <50108028+Landon5282@users.noreply.github.com>
2022-02-07 13:36:39 +0800

    Update urls.py

--- playCheseApp/urls.py
+++ playCheseApp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     url(r'make_size', views.make_size,),
     url(r'getpiecearray', views.get_piece_array,),
-    url(r'restart',views.restart,)
+    url(r'restart',views.restart,),
+    url(r'sendpos',views.sendPos,),
+    url(r'checkend',views.checkEnd,)
 ]
